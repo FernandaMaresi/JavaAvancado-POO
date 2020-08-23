e8b3ce2e4f7959b5bdce7b4a23059b94942ce13b
Fernanda Maresi <ferferborges@gmail.com>
2020-08-23 09:41:24 -0300

    Create Pessoa.java

---
+++ NetBeansProjects/Aula11/src/aula011/Pessoa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula011;
+
+/**
+ *
+ * @author ferfe
+ */
+public abstract class Pessoa {
+    protected String nome;
+    protected int idade;
+    protected String sexo;
+    
+    public void fazerAniversario(){
+        this.idade++;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Dados{" + "nome=" + nome + ", idade=" + idade + ", sexo=" + sexo + '}';
+    }
+    
+    
+}
