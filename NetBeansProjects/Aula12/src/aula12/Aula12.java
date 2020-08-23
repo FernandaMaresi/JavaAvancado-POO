35bd0e34254258d2c8208923564b261acad8d054
Fernanda Maresi <ferferborges@gmail.com>
2020-08-23 09:57:05 -0300

    Create Aula12.java

---
+++ NetBeansProjects/Aula12/src/aula12/Aula12.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula12;
+
+/**
+ *
+ * @author ferfe
+ */
+public class Aula12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //erro//Animal animal = new Animal();
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+        
+        Canguru c = new Canguru();
+        Cachorro k = new Cachorro();
+        Cobra j = new Cobra();
+        Tartaruga t = new Tartaruga();
+        Goldfish g = new Goldfish();
+        Arara e = new Arara();
+        
+        c.locomover();
+        k.locomover();
+        c.emitirSom();
+        k.emitirSom();
+        
+        
+        
+        
+        
+        
+       /* m.setPeso(35.3f);
+        m.setCorPelo("Marrom");
+        m.alimentar();
+        m.locomover();
+        m.emitirSom();
+        
+        a.locomover();
+        p.locomover();
+        r.locomover();*/
+        
+    }
+    
+}
