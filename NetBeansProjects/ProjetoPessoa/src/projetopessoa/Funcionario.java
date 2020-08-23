553be071d90e0613347cd4c8b9c15c7701247c01
Fernanda Maresi <ferferborges@gmail.com>
2020-08-23 10:01:17 -0300

    Create Funcionario.java

---
+++ NetBeansProjects/ProjetoPessoa/src/projetopessoa/Funcionario.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetopessoa;
+
+/**
+ *
+ * @author ferfe
+ */
+public class Funcionario extends Pessoa{
+    //Atributos
+    private String setor;
+    private boolean trabalhando;
+    
+    //Metodos
+    public void mudarTrabalho(){
+        
+    }
+    
+    //Metodos Especiais
+    public String getSetor(){
+        return this.setor;
+    }
+    public void setSetor(String setor){
+        this.setor = setor;
+    }
+
+    public boolean isTrabalhando() {
+        return trabalhando;
+    }
+
+    public void setTrabalhando(boolean trabalhando) {
+        this.trabalhando = trabalhando;
+    }
+    
+}
