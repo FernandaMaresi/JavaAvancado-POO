eb193e53285ab618e1da2d5619683feb3dcc41f4
Fernanda Maresi <ferferborges@gmail.com>
2020-08-23 09:47:14 -0300

    Create Mamifero.java

---
+++ NetBeansProjects/Aula13/src/aula13/Mamifero.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula13;
+
+/**
+ *
+ * @author ferfe
+ */
+public class Mamifero extends Animal{
+    protected String corPelo;
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som do Mamifero");
+    }
+}
