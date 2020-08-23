1654e1fac9ef3deac007dcceac4e66f4cf1edbb4
Fernanda Maresi <ferferborges@gmail.com>
2020-08-23 09:41:30 -0300

    Create Professor.java

---
+++ NetBeansProjects/Aula11/src/aula011/Professor.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula011;
+
+/**
+ *
+ * @author ferfe
+ */
+public class Professor extends Pessoa{
+    private String especialidade;
+    private double salario;
+    
+    public void receberAumento(){
+        this.salario++;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
