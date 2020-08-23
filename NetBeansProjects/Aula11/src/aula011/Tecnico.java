20dd7aeb825ee7f5ab9dfac9a974f9a0276e7354
Fernanda Maresi <ferferborges@gmail.com>
2020-08-23 09:41:38 -0300

    Create Tecnico.java

---
+++ NetBeansProjects/Aula11/src/aula011/Tecnico.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula011;
+
+/**
+ *
+ * @author ferfe
+ */
+public class Tecnico extends Aluno{
+    
+    public void registroProfissional(){
+        System.out.println("Registrado");
+    }
+    public void praticar(){
+        
+    }
+}
