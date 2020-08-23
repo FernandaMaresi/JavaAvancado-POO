b8e2936c1ad1ff674c79a2ff2800819ec5df4bd7
Fernanda Maresi <ferferborges@gmail.com>
2020-08-23 09:41:12 -0300

    Create Aula011.java

---
+++ NetBeansProjects/Aula11/src/aula011/Aula011.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula011;
+
+/**
+ *
+ * @author ferfe
+ */
+public class Aula011 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Pessoa p1 = new Pessoa();
+        Visitante v1 = new Visitante();
+        v1.setNome("Juvenal");
+        v1.setIdade(22);
+        v1.setSexo("M");
+        System.out.println(v1.toString());
+        
+        Aluno a1 = new Aluno();
+        a1.setNome("Claudio");
+        a1.setMatricula(1111);
+        a1.setCurso("Informática");
+        a1.setIdade(16);
+        a1.setSexo("M");
+        
+        System.out.println(a1.toString());
+        //pagar mensalidade do aluno
+        a1.pagarMensalidade();
+        
+        Bolsista b1 = new Bolsista();
+        b1.setMatricula(1112);
+        b1.setNome("Jubileu");
+        b1.setBolsa(12.5f);
+        b1.setSexo("M");
+        //pagar mensalidade do bolsista
+        b1.pagarMensalidade();
+       
+    }
+    
+}
