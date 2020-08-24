9e4ac43c902a7f59e63f50db6d7b4dcd09bad894
Fernanda Maresi <ferferborges@gmail.com>
2020-08-24 13:38:42 -0300

    Update Pessoa.java

--- NetBeansProjects/ProjetoLivro/src/projetolivro/Pessoa.java
+++ NetBeansProjects/ProjetoLivro/src/projetolivro/Pessoa.java
@@ -1,5 +1,7 @@
 package projetolivro;
 
+import java.util.Calendar;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +13,41 @@ package projetolivro;
  * @author ferfe
  */
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private String sexo;
+    
+    public Pessoa( String nome, int idade,String sexo){
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+       
+    }
+    public void fazerAniversario(){
+        this.idade++;
+        
+    }
+    
+    //Metodos Especiais
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
     
 }
