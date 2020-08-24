24fab75ee8cde179d8ef399e993b4f53d9c5512d
Fernanda Maresi <ferferborges@gmail.com>
2020-08-24 13:38:51 -0300

    Update Publicacao.java

--- NetBeansProjects/ProjetoLivro/src/projetolivro/Publicacao.java
+++ NetBeansProjects/ProjetoLivro/src/projetolivro/Publicacao.java
@@ -11,4 +11,10 @@ package projetolivro;
  */
 public interface Publicacao {
     
+    public abstract void abrir();
+    public abstract void fechar();
+    public abstract void folhear(int p);
+    public abstract void avancarPag();
+    public abstract void voltarPag();
+    
 }
