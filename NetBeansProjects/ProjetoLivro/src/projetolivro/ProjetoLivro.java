aac9af865554c963c6e0ebed446b84878a23cd2e
Fernanda Maresi <ferferborges@gmail.com>
2020-08-24 13:38:57 -0300

    Update ProjetoLivro.java

--- NetBeansProjects/ProjetoLivro/src/projetolivro/ProjetoLivro.java
+++ NetBeansProjects/ProjetoLivro/src/projetolivro/ProjetoLivro.java
@@ -16,6 +16,24 @@ public class ProjetoLivro {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //vetor
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro[3];
+        
+        p[0] = new Pessoa("Fernanda", 31, "F");
+        p[1] = new Pessoa("Pedro", 22, "M" );
+        
+        l[0]= new Livro("Aprendendo Java", "José da Silva", 300, p[0]);
+        l[1] = new Livro("POO para Iniciantes", "Pedro Paulo", 500, p[1]);
+        l[2] = new Livro("Java Avançado", "Maria Candido", 800, p[0]);
+        
+        l[0].abrir();
+        l[0].folhear(100);
+        l[0].avancarPag();
+        System.out.println(l[0].detalhes());
+        
+        System.out.println(l[1].detalhes());
+        
     }
     
 }
