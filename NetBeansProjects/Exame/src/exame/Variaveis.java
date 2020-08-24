342b587381574bbf3e8d6a55dbd4e342caa2103c
Fernanda Maresi <ferferborges@gmail.com>
2020-08-24 13:38:01 -0300

    Create Variaveis.java

---
+++ NetBeansProjects/Exame/src/exame/Variaveis.java
@@ -0,0 +1,16 @@
+package exame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ferfe
+ */
+public class Variaveis {
+    public static int x;
+            public int y;
+}
